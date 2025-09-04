afcae23e6e3e495e11bd5772bb2478bb89deba67
Galliyummy <jppanebia@gmail.com>
2025-09-04 13:31:57 -0400

    added file execution and test file

--- main.ts
+++ main.ts
@@ -2,23 +2,43 @@ import Parser from "./frontend/parser.ts";
 import Environment from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 
+const parser = new Parser();
+const env = new Environment();
+
+console.log("\nWelcome to jp-- V0.2");
+
+const fileInput = prompt("Enter a file name to run, or none to start REPL > ");
+
+if (fileInput) {
+    try {
+        const source = Deno.readTextFileSync(fileInput);
+        const program = parser.produceAST(source);
+        const result = evaluate(program, env);
+        console.log(result);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error("Error reading file:", err.message);
+        } else {
+            console.error("Unknown error:", err);
+        }
+    }
+}
+
 repl();
 
 function repl() {
-    const parser = new Parser();
-    const env = new Environment();
-    console.log("\nRepl V0.1");
-
+    console.log("\nStarting REPL...");
     while (true) {
         const input = prompt("> ");
-        //check for no user or exit keyword
-        if (!input || input.includes("exit")) {
-            Deno.exit(1);
+        if (!input || input.toLowerCase().includes("exit")) {
+            Deno.exit(0);
         }
-
-        const program = parser.produceAST(input);
-
-        const result = evaluate(program, env);
-        console.log(result);
+        try {
+            const program = parser.produceAST(input);
+            const result = evaluate(program, env);
+            console.log(result);
+        } catch (err) {
+            console.error("An error has occured:", err);
+        }
     }
 }
